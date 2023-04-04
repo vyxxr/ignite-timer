ae465f9d057d4a73af772be54e347cf277804900
vyxxr <vyctor@tuta.io>
2023-04-04 13:20:45 -0300

    add theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
-import { useState } from 'react'
+import { ThemeProvider } from 'styled-components'
 import { Button } from './components/Button'
+import { defaultTheme } from './styles/themes/default'
 
 export function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <Button variant='primary' />
       <Button variant='secondary' />
       <Button variant='danger' />
       <Button variant='success' />
       <Button />
-    </>
+    </ThemeProvider>
   )
 }
