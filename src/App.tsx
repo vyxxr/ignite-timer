389f95f0d3cf4c2cbcaf4211cefa7bf9b8e62366
vyxxr <vyctor@tuta.io>
2023-04-04 12:59:03 -0300

    add styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import { useState } from 'react'
+import { Button } from './components/Button'
 
 export function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <h1>Hello World</h1>
+    <>
+      <Button variant='primary' />
+      <Button variant='secondary' />
+      <Button variant='danger' />
+      <Button variant='success' />
+      <Button />
+    </>
   )
 }
