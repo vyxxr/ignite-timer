72720d2f875b05b588d755691556664ac141911b
vyxxr <vyctor@tuta.io>
2023-04-08 15:13:09 -0300

    finish home customization

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -15,11 +15,12 @@ export const GlobalStyle = createGlobalStyle`
   body {
     background: ${(props) => props.theme['gray-900']};
     color: ${(props) => props.theme['gray-300']};
+    -webkit-font-smoothing: antialiased;
   }
 
   body, input, textarea, button {
     font-family: 'Roboto', sans-serif;
     font-weight: 400;
-    fonr-size: 1rem;
+    font-size: 1rem;
   }
 `
